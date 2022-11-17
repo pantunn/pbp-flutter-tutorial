59c6c100d914d010235095c8e3613f03121b7ead
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2022-11-17 12:56:12 +0700

    maaf kak tadi salah push tugas 8 jadi lab 7 sy kehapus

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,56 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/drawer.dart';
+import 'package:flutter_application_1/form.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.brown,
-      ),
-      home: const MyHomePage(title: 'Program Counter 😃'),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+        );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    const MyHomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    final String title = 'Flutter Demo Home Page';
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+    @override
+    State<MyHomePage> createState() => _MyHomePageState();
 }
+
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String genap = 'genap ✌🏻';
-  String ganjil = 'ganjil ☝🏻'; 
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -59,15 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-    });
-  }
-
-  void _decrementCounter(){
-    setState(() {
-      if(_counter > 0) {
-        _counter--;
-      }
-      
     });
   }
 
@@ -81,11 +55,36 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      drawer: const MyDrawer(),
+      // Menambahkan drawer menu
+      drawer: Drawer(
+          child: Column(
+            children: [
+              // Menambahkan clickable menu
+              ListTile(
+                title: const Text('Counter'),
+                onTap: () {
+                  // Route menu ke halaman utama
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Form'),
+                onTap: () {
+                  // Route menu ke halaman form
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyFormPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -106,56 +105,21 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            if (_counter % 2 == 0)... [
-              const Text('GENAP',
-              style:TextStyle(color: Colors.red),), 
-            ]else...[
-              const Text('GANJIL',
-              style: TextStyle(color: Colors.blue),), 
-            ],
+            const Text(
+              'You have clicked the button this many times:',
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
-          
         ),
       ),
-      
-      floatingActionButton: Padding(
-        padding: EdgeInsets.only(left: 30),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            if (_counter == 0)... {
-              
-              Expanded(child: Container()),
-              FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-            }
-            else... {
-              FloatingActionButton(
-              
-              onPressed: _decrementCounter,
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-              Expanded(child: Container()),
-              FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-            }
-            
-          ],
-        )
-        
-      )
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
